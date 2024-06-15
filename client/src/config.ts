44f11d7e736050f6824f35cf7383a42589387cf8
Villy15 <adrianvill07@gmail.com>
2024-06-15 16:19:55 +0800

    Fix env config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env.VITE_NODE_ENV === 'production'
-    ? import.meta.env.VITE_NODE_ENV
-    : import.meta.env.VITE_NODE_ENV;
+    ? import.meta.env.VITE_API_URL_PROD
+    : import.meta.env.VITE_API_URL_DEV;
 
 export default API_URL;
