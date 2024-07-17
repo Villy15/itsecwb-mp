993f90c92556c3905bf2915cceec8a4c34b46137
Villy15 <adrianvill07@gmail.com>
2024-07-17 18:03:58 +0800

    Fix cookies render

--- server/server.js
+++ server/server.js
@@ -58,16 +58,18 @@ app.use(
 
 app.use(
   session({
-    secret: "your-secret-key",
+    // openssl rand -hex 32
+    secret: process.env.SESSION_KEY,
     resave: false,
     saveUninitialized: true,
+    rolling: true, // Reset the session maxAge on every request
     proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
     name: "itsecwb-session", // This will be unique per-host.
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
       secure: process.env.NODE_ENV === "production" ? true : false,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for production, 'lax' for other environments
-      httpOnly: false,
+      httpOnly: process.env.NODE_ENV === "production" ? true : false,
     },
   })
 );
