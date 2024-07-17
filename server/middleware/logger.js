e1d519447de9e3f5f64997551b9c2fee57fe87dc
theHotGod <anthony_paguio@dlsu.edu.ph>
2024-07-17 15:36:54 +0800

    testing logger to log file

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,4 +1,14 @@
 import colors from "colors";
+// import fs for file system
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// create a __dirname variable to store the current directory
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// create a logFilePath so that we can write logs to a file
+const logFilePath = path.join(__dirname, "../logs/access.log");
 
 const logger = (req, res, next) => {
   const methodColors = {
@@ -9,6 +19,11 @@ const logger = (req, res, next) => {
   };
 
   const color = methodColors[req.method] || colors.white;
+  // create a log message
+  const logMessage = `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`;
+
+  // write the log message to the log file
+  fs.appendFileSync(logFilePath, color(logMessage) + "\n");
 
   console.log(
     color(
